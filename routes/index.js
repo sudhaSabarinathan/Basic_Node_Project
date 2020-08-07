53e36ad29530bc8bdff6bd3d564c78ee1c931b99
sudhaSabarinathan <sudha.m@adappt.co>
2020-08-07 13:36:01 +0530

    added semicolon and used public static folder

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,16 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 
-router.get('/', (req,res)=>{
-  res.render('index');
-})
+router.get('/', (req, res) => {
+    res.render('index');
+});
 
-router.get('/aboutus', (req,res)=>{
-  res.render('aboutus', {pagename:'AboutUs'}); 
-})
+router.get('/aboutus', (req, res) => {
+    res.render('aboutus', { pagename: 'AboutUs' });
+});
 
-module.exports = router
+router.get('/*', (req, res) => {
+    res.render('pageNotFound');
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,18 +1,17 @@
-const express = require('express')
-const app = express()
-const expressLayout = require('express-ejs-layouts')
+const express = require('express');
+const app = express();
+const expressLayout = require('express-ejs-layouts');
 const mongoose = require('mongoose');
 
-const pageRouter = require('./routes/index') 
+const pageRouter = require('./routes/index');
 
-app.set('view engine', 'ejs')
-app.set('views', __dirname + '/views')
-app.set('layout', 'layouts/layout')
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.set('layout', 'layouts/layout');
 
-app.use(expressLayout)
-app.use(express.static('public'))
-
-//mongoose.connect()
+app.use(expressLayout);
+app.use('/files', express.static('public'));
 app.use('/', pageRouter);
 
-app.listen(process.env.Port || 4000)
+require('dotenv').config();
+module.exports = app;
